175de3f87ea8aa4e5276bb2970cfc6e41adbdb73
Neil197 <148237043+Neil197@users.noreply.github.com>
2025-01-22 22:37:29 +0000

    Update custom.php

--- custom.php
+++ custom.php
@@ -17,14 +17,18 @@ function preload_lcp_images_by_id() {
 
     // Define page IDs and their respective LCP images
     $preload_images = array(
-        12   => '/wp-content/uploads/2023/04/Divi-accountant-banner-1920x864-1.webp'  // Home page
+        12   => '/wp-content/uploads/2023/04/Divi-accountant-banner-1920x864-1.webp',      // Home page
+        561 => '/wp-content/uploads/2023/04/CC0-about-us-bannner-1800x450-1.webp'        // About Us
+        1292 => '/wp-content/uploads/2023/04/CC0-terms-of-service-banner-1040x345-1.webp'    // Terms of Service
     );
 
     $current_post_id = get_queried_object_id();
 
     if (isset($preload_images[$current_post_id])) {
         $image_url = esc_url($preload_images[$current_post_id]);
-        echo "<link rel='preload' as='image' href='{$image_url}' />\\n";
+        echo "<!-- Preload LCP Images -->\n";
+        echo "<link rel='preload' as='image' href='{$image_url}' />\n";
+        echo "<!-- End Preload LCP Images -->\n";
     }
 }
 add_action('wp_head', 'preload_lcp_images_by_id', 1);
